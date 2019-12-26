c6ca1dc6822e29e7ff8e892dd981d893cb53b519
Vadzim Marchanka <chakapega@gmail.com>
2019-12-26 09:32:55 +0300

    feat: added eslint

--- simple-piskel-clone/src/App.js
+++ simple-piskel-clone/src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from "react";
-import Header from "./components/Header";
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <Fragment>
-      <Header></Header>
+      <Header />
     </Fragment>
   );
 };
--- simple-piskel-clone/src/components/Header.js
+++ simple-piskel-clone/src/components/Header.js
@@ -4,9 +4,11 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <a className="header-link" href="#">Simle Piskel Clone</a>
+      <a className="header-link" href="#">
+        Simle Piskel Clone
+      </a>
     </header>
   );
-}
+};
 
 export default Header;
