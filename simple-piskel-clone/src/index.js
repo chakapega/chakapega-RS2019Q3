9e347086a3738f1d131cfa0f9e9f7329c31db70d
Vadzim Marchanka <chakapega@gmail.com>
2019-12-26 06:06:19 +0300

    feat: initial header is made; editorconfig added; font and normalize styles added

---
+++ simple-piskel-clone/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// import './normalize/normalize.scss';
+// import './font/font.scss';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
