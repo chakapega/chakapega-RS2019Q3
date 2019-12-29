5b2d8d0d35d62d6c96eae9ed20fa69c2d15fff77
Vadzim Marchanka <chakapega@gmail.com>
2019-12-29 10:31:54 +0300

    feat: added PenSizeSelector component

--- simple-piskel-clone/src/components/leftControlUnit/PenSizeSelector.js
+++ simple-piskel-clone/src/components/leftControlUnit/PenSizeSelector.js
@@ -3,28 +3,49 @@ import React, { Component } from 'react';
 import './PenSizeSelector.scss';
 
 export default class PenSizeSelector extends Component {
+  constructor() {
+    super();
+
+    this.activeButton = 'first';
+  }
+
+  componentDidMount() {
+    document.querySelector(`.pen-size-button_${this.activeButton}`).classList.add('pen-size-button_active');
+  }
+
+  changeSize = event => {
+    const selectedSize = event.target.getAttribute('aria-label');
+
+    document.querySelector('.pen-size-button_active').classList.remove('pen-size-button_active');
+    document.querySelector(`.pen-size-button_${selectedSize}`).classList.add('pen-size-button_active');
+  };
+
   render() {
     return (
       <div className='pen-size-selector'>
         <button
           className='pen-size-button pen-size-button_first'
           type='button'
-          aria-label='1'
+          aria-label='first'
+          onClick={this.changeSize}
         />
         <button
-          className='pen-size-button pen-size-button_active pen-size-button_second'
+          className='pen-size-button pen-size-button_second'
           type='button'
-          aria-label='2'
+          aria-label='second'
+          onClick={this.changeSize}
         />
         <button
           className='pen-size-button pen-size-button_third'
           type='button'
-          aria-label='3'
+          aria-label='third'
+          onClick={this.changeSize}
         />
         <button
           className='pen-size-button pen-size-button_fourth'
           type='button'
-          aria-label='4'
+          aria-label='fourth'
+          onClick={this.changeSize}
         />
       </div>
     );
