5891e1c3897002e9e7b391c9106e71e4798b7296
Vadzim Marchanka <chakapega@gmail.com>
2020-01-03 19:51:16 +0300

    refactor: application structure changed

--- simple-piskel-clone/src/index.js
+++ simple-piskel-clone/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './normalize/normalize.scss';
-import './font/font.scss';
+import './assets/normalize/normalize.scss';
+import './assets/font/font.scss';
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
