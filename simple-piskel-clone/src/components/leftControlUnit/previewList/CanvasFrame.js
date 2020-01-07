f3362c3f2a4333041b37be0f2b0f097008c9da6d
Vadzim Marchanka <chakapega@gmail.com>
2020-01-08 01:43:29 +0300

    feat: added display of buttons on the canvas frame on which the cursor is hovering over

--- simple-piskel-clone/src/components/leftControlUnit/previewList/CanvasFrame.js
+++ simple-piskel-clone/src/components/leftControlUnit/previewList/CanvasFrame.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './CanvasFrame.scss';
 
-const CanvasFrame = ({ id, isActive, changeActiveCanvasFrame, deleteCanvasFrame }) => {
-  const className = isActive
-    ? 'preview-list-canvas-container preview-list-canvas-container_active'
-    : 'preview-list-canvas-container';
+class CanvasFrame extends Component {
+  showHiddenButtonsOnCanvasFrame = id => {
+    document
+      .querySelector(`#delete-canvas-button_${id}`)
+      .classList.remove('delete-canvas-frame-button-container_hidden');
+  };
 
-  return (
-    <li className='canvas-frame'>
-      <button type='button' className={className} onClick={() => changeActiveCanvasFrame(id)}>
-        <canvas id={`preview-canvas_${id}`} className='preview-list-canvas' />
-      </button>
-      <div className='delete-canvas-frame-icon-container'>
+  hideButtonsOnCanvasFrame = id => {
+    document.querySelector(`#delete-canvas-button_${id}`).classList.add('delete-canvas-frame-button-container_hidden');
+  };
+
+  render() {
+    const { id, isActive, changeActiveCanvasFrame, deleteCanvasFrame } = this.props;
+    const className = isActive
+      ? 'preview-list-canvas-container preview-list-canvas-container_active'
+      : 'preview-list-canvas-container';
+
+    return (
+      <li className='canvas-frame'>
         <button
           type='button'
-          className='delete-canvas-frame-button'
-          aria-label='delete frame'
-          onClick={() => deleteCanvasFrame(id)}
-        />
-      </div>
-    </li>
-  );
-};
+          className={className}
+          onClick={() => changeActiveCanvasFrame(id)}
+          onMouseEnter={() => this.showHiddenButtonsOnCanvasFrame(id)}
+          onMouseLeave={() => this.hideButtonsOnCanvasFrame(id)}
+        >
+          <canvas id={`preview-canvas_${id}`} className='preview-list-canvas' />
+        </button>
+        <div
+          id={`delete-canvas-button_${id}`}
+          className='delete-canvas-frame-button-container delete-canvas-frame-button-container_hidden'
+          onMouseEnter={() => this.showHiddenButtonsOnCanvasFrame(id)}
+          onMouseLeave={() => this.hideButtonsOnCanvasFrame(id)}
+        >
+          <button
+            type='button'
+            className='delete-canvas-frame-button'
+            aria-label='delete frame'
+            onClick={() => deleteCanvasFrame(id)}
+          />
+        </div>
+      </li>
+    );
+  }
+}
 
 CanvasFrame.propTypes = {
   id: PropTypes.number.isRequired,
