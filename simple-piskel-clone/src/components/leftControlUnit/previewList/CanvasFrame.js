aba8ab6711f702f0c5c71efd3ff089501cda188f
Vadzim Marchanka <chakapega@gmail.com>
2020-01-09 03:35:10 +0300

    feat: added duplicate canvas frame button

--- simple-piskel-clone/src/components/leftControlUnit/previewList/CanvasFrame.js
+++ simple-piskel-clone/src/components/leftControlUnit/previewList/CanvasFrame.js
@@ -9,14 +9,22 @@ class CanvasFrame extends Component {
 
     if (arrayOfCanvasFrames.length > 1)
       document
-        .querySelector(`#delete-canvas-button_${id}`)
+        .querySelector(`#delete-canvas-frame-button-container_${id}`)
         .classList.remove('delete-canvas-frame-button-container_hidden');
+    document
+      .querySelector(`#duplicate-canvas-frame-button-container_${id}`)
+      .classList.remove('duplicate-canvas-frame-button-container_hidden');
   };
 
   hideButtonsOnCanvasFrame = () => {
     const { id } = this.props;
 
-    document.querySelector(`#delete-canvas-button_${id}`).classList.add('delete-canvas-frame-button-container_hidden');
+    document
+      .querySelector(`#delete-canvas-frame-button-container_${id}`)
+      .classList.add('delete-canvas-frame-button-container_hidden');
+    document
+      .querySelector(`#duplicate-canvas-frame-button-container_${id}`)
+      .classList.add('duplicate-canvas-frame-button-container_hidden');
   };
 
   render() {
@@ -35,14 +43,26 @@ class CanvasFrame extends Component {
           <canvas id={`preview-canvas_${id}`} className='preview-list-canvas' />
         </button>
         <div
-          id={`delete-canvas-button_${id}`}
+          id={`delete-canvas-frame-button-container_${id}`}
           className='delete-canvas-frame-button-container delete-canvas-frame-button-container_hidden'
         >
           <button
             type='button'
             className='delete-canvas-frame-button'
             aria-label='delete frame'
+            title='delete frame'
             onClick={() => deleteCanvasFrame(id)}
+          />
+        </div>
+        <div
+          id={`duplicate-canvas-frame-button-container_${id}`}
+          className='duplicate-canvas-frame-button-container duplicate-canvas-frame-button-container_hidden'
+        >
+          <button
+            type='button'
+            className='duplicate-canvas-frame-button'
+            aria-label='duplicate frame'
+            title='duplicate frame'
           />
         </div>
       </li>
