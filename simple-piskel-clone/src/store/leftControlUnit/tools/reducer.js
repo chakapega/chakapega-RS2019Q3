b74b8f5d358c4f255bae78f5754c1109ad430a76
Vadim <chakapega@gmail.com>
2020-01-11 21:38:12 +0300

    feat: implemented saving of user session in localStorage, except for canvas states

--- simple-piskel-clone/src/store/leftControlUnit/tools/reducer.js
+++ simple-piskel-clone/src/store/leftControlUnit/tools/reducer.js
@@ -14,24 +14,29 @@ import {
 } from './actionsTypes';
 
 const initialState = {
-  activeToolSize: toolSizeOne,
-  activeTool: toolPen,
-  activeCanvasSize: canvasSizeThirtyTwo,
-  activeFirstCanvasColor: blackCanvasColor,
-  activeSecondCanvasColor: whiteCanvasColor
+  activeToolSize: localStorage.getItem('activeToolSize') || toolSizeOne,
+  activeTool: localStorage.getItem('activeTool') || toolPen,
+  activeCanvasSize: localStorage.getItem('activeCanvasSize') || canvasSizeThirtyTwo,
+  activeFirstCanvasColor: localStorage.getItem('activeFirstCanvasColor') || blackCanvasColor,
+  activeSecondCanvasColor: localStorage.getItem('activeSecondCanvasColor') || whiteCanvasColor
 };
 
 const leftControlUnitReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_TOOL_SIZE:
+      localStorage.setItem('activeToolSize', action.payload);
       return { ...state, activeToolSize: action.payload };
     case CHANGE_TOOL:
+      localStorage.setItem('activeTool', action.payload);
       return { ...state, activeTool: action.payload };
     case CHANGE_CANVAS_SIZE:
+      localStorage.setItem('activeCanvasSize', action.payload);
       return { ...state, activeCanvasSize: action.payload };
     case CHANGE_FIRST_CANVAS_COLOR:
+      localStorage.setItem('activeFirstCanvasColor', action.payload);
       return { ...state, activeFirstCanvasColor: action.payload };
     case CHANGE_SECOND_CANVAS_COLOR:
+      localStorage.setItem('activeSecondCanvasColor', action.payload);
       return { ...state, activeSecondCanvasColor: action.payload };
     default:
       return state;
--- simple-piskel-clone/src/store/rightControlUnit/reducer.js
+++ simple-piskel-clone/src/store/rightControlUnit/reducer.js
@@ -2,12 +2,13 @@ import CHANGE_FPS_PREVIEW_ANIMATION from './actionsTypes';
 import { previewAnimationFpsTen } from '../../shared/constants';
 
 const initialState = {
-  activePreviewAnimationFps: previewAnimationFpsTen
+  activePreviewAnimationFps: +localStorage.getItem('activePreviewAnimationFps') || previewAnimationFpsTen
 };
 
 const rightControlUnitReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_FPS_PREVIEW_ANIMATION:
+      localStorage.setItem('activePreviewAnimationFps', action.payload);
       return { ...state, activePreviewAnimationFps: action.payload };
     default:
       return state;
